0b068baea5913826e3505e146a323c89766a5458
Perjési Ákos <akos.perjesi@gmail.com>
2017-06-25 19:45:50 +0200

    First plot

--- calc.py
+++ calc.py
@@ -1,1 +1,6 @@
 print(1500000 * 1.022 ** 8)
+height=1.7
+weight=95
+BMI=weight/height**2
+
+print(BMI)
--- import_csv.py
+++ import_csv.py
@@ -5,3 +5,13 @@ with open('MSFT.csv') as csvfile:
 
     mylist = list(readCSV)
     print(mylist)
+
+import csv
+
+with open('MSFT.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        print(row)
+        print(row[0])
+        print(row[0], row[1], row[2], )
+        import csv
