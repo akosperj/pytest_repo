30491327ef3513d7358dac7a7085743f642efdc5
Perjési Ákos <akos.perjesi@gmail.com>
2017-06-27 20:49:00 +0200

    plottcommit

--- boktest.py
+++ boktest.py
@@ -1,10 +1,13 @@
-# create a new plot with default tools, using figure
+from bokeh.plotting import figure, output_file, show
 
-from bokeh.io import output_notebook, show
-from bokeh.plotting import figure
-p = figure(plot_width=400, plot_height=400)
+import pandas as pd
+import numpy as np
+import csv
 
-# add a circle renderer with a size, color, and alpha
-p.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], size=15, line_color="navy", fill_color="orange", fill_alpha=0.5)
+df = pd.read_csv("MSFT.csv")
 
-show(p) # show the results
+print(df)
+
+
+df.plot()
+
